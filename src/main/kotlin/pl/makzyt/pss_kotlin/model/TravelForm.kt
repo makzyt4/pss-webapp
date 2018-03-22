2dad2f23cceea80d00f3bb7800e7aaf50493ffa1
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-22 20:07:42 +0100

    Removed redundancy in layout.html head tag

--- src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelForm.kt
+++ src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelForm.kt
@@ -9,12 +9,12 @@ import javax.validation.constraints.NotNull
 @CorrectDates(message = "{validation.incorrectDates}")
 class TravelForm {
 
-    @NotNull(message = "{validation.enterData}")
+    @NotNull(message = "{validation.emptyData}")
     @PresentOrFuture(message = "{validation.futureDate}")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     var departureDate: Date? = null
 
-    @NotNull(message = "{validation.enterData}")
+    @NotNull(message = "{validation.emptyData}")
     @PresentOrFuture(message = "{validation.futureDate}")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     var returnDate: Date? = null
