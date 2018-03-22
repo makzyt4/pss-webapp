95520902d253b6f9c4bd0748c9317524728d0b74
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-22 15:14:00 +0100

    Made first POST request for DepartmentDate

--- src/main/kotlin/pl/makzyt/pss_kotlin/controller/FormController.kt
+++ src/main/kotlin/pl/makzyt/pss_kotlin/controller/FormController.kt
@@ -1,18 +1,39 @@
 package pl.makzyt.pss_kotlin.controller
 
+import org.springframework.beans.propertyeditors.CustomDateEditor
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.WebDataBinder
+import org.springframework.web.bind.annotation.*
+import pl.makzyt.pss_kotlin.model.TravelForm
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.validation.Valid
 
 @Controller
+@RequestMapping("/travel")
 open class GreetingController {
 
-    @GetMapping("/travel/results")
-    fun results(): String {
-        return "results"
+    @InitBinder
+    fun bindingPreparation(binder: WebDataBinder) {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val orderDateEditor: CustomDateEditor = CustomDateEditor(dateFormat, true)
+        binder.registerCustomEditor(Date::class.java, orderDateEditor)
     }
 
-    @GetMapping("/travel/form")
-    fun form(): String {
+    @GetMapping("/form")
+    fun form(model: Model): String {
+        model.addAttribute("form", TravelForm())
         return "travelform"
+    }
+
+    @PostMapping("/form")
+    fun results(@Valid @ModelAttribute("form") form: TravelForm, result: BindingResult): String {
+        if (result.hasErrors()) {
+            return "travelform"
+        }
+
+        return "results"
     }
 }
