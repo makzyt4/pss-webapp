e6039116d4e8d54c77c7e71d44271da001857b28
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-23 20:18:49 +0100

    Added third input, removed bottom margin from datetime pickers

--- src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelForm.kt
+++ src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelForm.kt
@@ -4,18 +4,24 @@ import org.springframework.format.annotation.DateTimeFormat
 import pl.makzyt.pss_kotlin.validator.CorrectDates
 import pl.makzyt.pss_kotlin.validator.PresentOrFuture
 import java.util.*
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.FutureOrPresent
 import javax.validation.constraints.NotNull
 
 @CorrectDates(message = "{validation.incorrectDates}")
 class TravelForm {
 
     @NotNull(message = "{validation.emptyData}")
-    @PresentOrFuture(message = "{validation.futureDate}")
+    @FutureOrPresent(message = "{validation.futureDate}")
     @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
     var departureDate: Date? = null
 
     @NotNull(message = "{validation.emptyData}")
-    @PresentOrFuture(message = "{validation.futureDate}")
+    @FutureOrPresent(message = "{validation.futureDate}")
     @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
     var returnDate: Date? = null
+
+    @NotNull(message = "{validation.emptyData}")
+    @DecimalMin(value = "0", message = "{validation.negativeCost}")
+    var dietCost: Float? = null
 }
