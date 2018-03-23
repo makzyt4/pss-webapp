8ccd02649e3a0685e26c06980e50c55efea846af
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-23 23:40:52 +0100

    Added travel time counting

--- src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelResult.kt
+++ src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelResult.kt
@@ -5,4 +5,13 @@ import java.text.SimpleDateFormat
 class TravelResult(form: TravelForm) {
     val depDate = SimpleDateFormat("dd.MM.yyyy").format(form.departureDate) + ", godz." + SimpleDateFormat("HH:mm").format(form.departureDate)
     val retDate = SimpleDateFormat("dd.MM.yyyy").format(form.returnDate) + ", godz." + SimpleDateFormat("HH:mm").format(form.returnDate)
+    val travelTimeInSec = (form.returnDate!!.time - form.departureDate!!.time) / 1000f
+    val travelTimeAllMinutes: Int = (travelTimeInSec / 60).toInt()
+    val travelTimeHours: Int = (travelTimeInSec / 3600).toInt()
+    val travelTimeMinutes: Int = travelTimeAllMinutes - travelTimeHours * 60
+    val travelTime = String.format("%d g %d min", travelTimeHours, travelTimeMinutes)
+
+    val timeCost = (180f / 7f / 24f / 3600f) * (form.returnDate!!.time - form.departureDate!!.time) / 1000f
+
+    val cost = String.format("%.2f zł", timeCost)
 }
