29b1ff72fce4c651a29f74b39e62366326478311
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-03-23 01:00:54 +0100

    Changed date format in results

--- src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelResult.kt
+++ src/main/kotlin/pl/makzyt/pss_kotlin/model/TravelResult.kt
@@ -3,6 +3,6 @@ package pl.makzyt.pss_kotlin.model
 import java.text.SimpleDateFormat
 
 class TravelResult(form: TravelForm) {
-    val depDate = SimpleDateFormat("dd.MM.yyyy HH:mm").format(form.departureDate)
-    val retDate = SimpleDateFormat("dd.MM.yyyy HH:mm").format(form.returnDate)
+    val depDate = SimpleDateFormat("dd.MM.yyyy").format(form.departureDate) + ", godz." + SimpleDateFormat("HH:mm").format(form.departureDate)
+    val retDate = SimpleDateFormat("dd.MM.yyyy").format(form.returnDate) + ", godz." + SimpleDateFormat("HH:mm").format(form.returnDate)
 }
