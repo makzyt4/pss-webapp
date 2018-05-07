e9fa02ed0349572586a4107e4e81893244783f69
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-05-07 19:59:45 +0200

    NIP validation

--- src/main/kotlin/pl/makzyt/psskotlin/model/SignUpForm.kt
+++ src/main/kotlin/pl/makzyt/psskotlin/model/SignUpForm.kt
@@ -4,12 +4,14 @@ import org.hibernate.validator.constraints.Length
 import pl.makzyt.psskotlin.validator.PasswordRepeated
 import pl.makzyt.psskotlin.validator.UniqueLogin
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
 
 @PasswordRepeated(message = "{validation.passwordNotRepeated}")
 class SignUpForm {
     @NotEmpty(message = "{validation.emptyData}")
     var companyName: String = ""
     @NotEmpty(message = "{validation.emptyData}")
+    @Pattern(message = "{validation.incorrectNIP}", regexp = "\\d{10}")
     var nip: String = ""
     @NotEmpty(message = "{validation.emptyData}")
     @UniqueLogin(message = "{validation.loginNotUnique}")
