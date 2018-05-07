ebcdb8a480751fe21fe4ce7c1feb838d9a88ed3b
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-05-07 18:47:43 +0200

    AppUser repository

---
+++ src/main/kotlin/pl/makzyt/psskotlin/repository/AppUserRepository.kt
@@ -0,0 +1,10 @@
+package pl.makzyt.psskotlin.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import pl.makzyt.psskotlin.model.AppUser
+
+@Repository
+interface AppUserRepository : JpaRepository<AppUser, Long> {
+    fun findByLogin(login: String)
+}
